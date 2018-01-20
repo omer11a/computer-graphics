c89784102ad6e20a5923a02b902b355f28232d1d
omer <omer@OMERD>
2018-01-20 20:49:46 +0200

    Added wood texture

--- CG_skel_w_MFC/fshader_texture.glsl
+++ CG_skel_w_MFC/fshader_texture.glsl
@@ -15,6 +15,7 @@ uniform bool hasNormalMapping;
 uniform bool hasFog;
 uniform bool hasColorAnimation;
 uniform bool hasToonShading;
+uniform bool hasWoodTexture;
 uniform vec3 cameraPosition;
 uniform vec3 ambientLightColor;
 uniform int numberOfLights;
@@ -27,6 +28,8 @@ uniform float inScatteringCoefficient;
 uniform int colorAnimationRepresentation;
 uniform float colorAnimationDelta;
 uniform int colorQuantizationCoefficient;
+uniform vec3 woodColor1;
+uniform vec3 woodColor2;
 
 layout (location = 0) in vec3 vertexPosition;
 layout (location = 1) in vec3 vertexNormal;
@@ -55,6 +58,21 @@ vec3 hsv2rgb(vec3 c) {
     vec4 k = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
     vec3 p = abs(fract(c.xxx + k.xyz) * 6.0 - k.www);
     return c.z * mix(k.xxx, clamp(p - k.xxx, 0.0, 1.0), c.y);
+}
+
+float random(vec2 seed) {
+	return fract(sin(dot(seed.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+float noise(vec2 seed) {
+    vec2 i = floor(seed);
+    vec2 f = fract(seed);
+    vec2 u = f * f * (3.0 - 2.0 * f);
+    return mix(
+		mix(random(i), random(i + vec2(1.0, 0.0)), u.x),
+		mix(random(i + vec2(0.0, 1.0)), random(i + vec2(1.0, 1.0)), u.x),
+		u.y
+	);
 }
 
 vec3 applyColorAnimation(vec3 color, int representation, float delta) {
@@ -114,6 +132,17 @@ void main() {
 		vec3 diffuseColor = diffuseReflectance;
 		if (hasTexture) {
 			ambientColor = texture(textureSampler, uv).rgb;
+			specularColor = ambientColor;
+			diffuseColor = ambientColor;
+		}
+
+		if (hasWoodTexture) {
+			vec2 st = vertexPosition.xy / 1000.0;
+			vec2 position = st.yx * vec2(20, 5);
+			float angle = noise(position) * 0.7;
+			position = mat2(cos(angle), - sin(angle), sin(angle), cos(angle)) * position;
+			float pattern = smoothstep(0.0, 1.0, abs((sin(position.x * 3.1415) + 1)) * 0.5);
+			ambientColor = pattern * woodColor1 + (1 - pattern) * woodColor2;
 			specularColor = ambientColor;
 			diffuseColor = ambientColor;
 		}
