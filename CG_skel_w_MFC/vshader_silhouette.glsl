117e47707d9974eb50a718e4637ebddbe0259247
omer <omer@OMERD>
2018-01-20 13:24:26 +0200

    Added silhouette shaders

---
+++ CG_skel_w_MFC/vshader_silhouette.glsl
@@ -0,0 +1,15 @@
+#version 400
+
+uniform mat4 modelMatrix;
+uniform mat3 normalModelMatrix;
+uniform mat4 viewProjectionMatrix;
+uniform float silhouetteThickness;
+
+layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec3 vertexNormal;
+
+void main() {
+    vec3 transformedVertex = (modelMatrix * vec4(vertexPosition, 1)).xyz;
+	vec3 transformedNormal = silhouetteThickness * normalize(normalModelMatrix * vertexNormal);
+	gl_Position = viewProjectionMatrix * vec4(transformedVertex + transformedNormal, 1);
+}
