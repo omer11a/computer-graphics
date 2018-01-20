f5c3deea4cee0b66a6e7dcf169bd6a7ec49f9eaf
omer <omer@OMERD>
2018-01-20 19:34:29 +0200

    Fixed toon shading

--- CG_skel_w_MFC/vshader_silhouette.glsl
+++ CG_skel_w_MFC/vshader_silhouette.glsl
@@ -1,7 +1,7 @@
 #version 400
 
 uniform mat4 modelMatrix;
-uniform mat3 normalModelMatrix;
+uniform mat3 normalMatrix;
 uniform mat4 viewProjectionMatrix;
 uniform float silhouetteThickness;
 
@@ -10,6 +10,6 @@ layout(location = 1) in vec3 vertexNormal;
 
 void main() {
     vec3 transformedVertex = (modelMatrix * vec4(vertexPosition, 1)).xyz;
-	vec3 transformedNormal = silhouetteThickness * normalize(normalModelMatrix * vertexNormal);
+	vec3 transformedNormal = silhouetteThickness * normalize(normalMatrix * vertexNormal);
 	gl_Position = viewProjectionMatrix * vec4(transformedVertex + transformedNormal, 1);
 }
